af256be8777ccbd255afbf5fc41c51cb648b06fb
Samuel Bedoya <samy040902@gmail.com>
2024-10-19 17:49:09 -0500

    Código como lo tenía el profe

--- Assets/Script/Tts.cs
+++ Assets/Script/Tts.cs
@@ -5,18 +5,25 @@ using SpeechLib;
 
 public class Tts : MonoBehaviour
 {
+    public string sayit = "Que bonitos ojos tienes, quiero chuparte el pepe";
     public SpVoice voice = new SpVoice();
     // Start is called before the first frame update
     void Start()
     {
-        voice.Speak("Buenas tardes", SpeechVoiceSpeakFlags.SVSFlagsAsync | SpeechVoiceSpeakFlags.SVSFPurgeBeforeSpeak);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Speak(sayit);
+        }
     }
 
-    //void Speak
+    void Speak(string text)
+    {
+        voice.Speak(text, SpeechVoiceSpeakFlags.SVSFlagsAsync | SpeechVoiceSpeakFlags.SVSFPurgeBeforeSpeak);
+    }
 }
